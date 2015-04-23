39bda1ca1cdad277698b95a450963923d49f3321
Nalleyer <nalleyer@gmail.com>
2015-04-23 21:54:18 +0800

    finished techtree.cpp

---
+++ Classes/TechTree.cpp
@@ -0,0 +1,103 @@
+#include "TechTree.h"
+
+using namespace cocos2d;
+using techTree::mTechStringEnumMap;
+void TechTree::addTechInit(TechEnum tech, const std::vector<TechEnum> & fathers)
+{
+	std::vector<int> indexFather;
+	for (auto father : fathers)
+	{
+		for (int i = 0; i < mTechTree.size(); ++i)
+		{
+			if (mTechTree[i].techName == father)
+			{
+				indexFather.push_back(i);
+			}
+		}
+	}
+	if (indexFather.empty())
+	{
+		//error
+	}
+	mTechTree.push_back(TechTreeNodeStruct{ tech, indexFather, false });
+}
+
+TechTree::TechTree()
+{
+	//init here
+	mTechTree.clear();
+	mTechTree.push_back(TechTreeNodeStruct{ techroot, std::vector<int>{0}, false });
+	//get json file
+	auto jsonFile = FileUtils::getInstance()->fullPathForFilename("dictionary/techtree.json");
+    CCLOG("%s",jsonFile.c_str());
+	ssize_t size = 0;
+	unsigned char * loadStr = FileUtils::getInstance()->getFileData(jsonFile, "r", &size);
+	std::string jsonStr = std::string((const char *)loadStr, size);
+	//json
+	rapidjson::Document document;
+	document.Parse<0>(jsonStr.c_str());
+	if (document.HasParseError())
+	{
+		CCLOG("json parsing error!");
+	}
+	if (!document.IsObject())
+	{
+		CCLOG("not object");
+	}
+	rapidjson::Value & techs = document["techs"];
+	if (techs.IsArray())
+	{
+		for (int i = 0; i < techs.Size(); ++i)
+		{
+			CCLOG("this:  %s", techs[i]["tech"].GetString());
+			rapidjson::Value & fathers = techs[i]["fathers"];
+			std::vector<TechEnum> fathersList;
+			if (fathers.IsArray())
+			{
+				for (int j = 0; j < fathers.Size(); ++j)
+				{
+					if (fathers[j].IsString())
+					{
+						CCLOG("fathers: %s", fathers[j].GetString());
+						fathersList.push_back(mTechStringEnumMap.at(std::string{ fathers[j].GetString() }));
+					}
+				}
+			}
+			addTechInit( mTechStringEnumMap.at(techs[i]["tech"].GetString()),fathersList);
+		}
+	}
+}
+
+TechTree::~TechTree()
+{
+}
+
+void TechTree::unlock(TechEnum tech)
+{
+	for (auto i : mTechTree)
+	{
+		if (i.techName == tech)
+		{
+			i.isUnclocked = true;
+			return;
+		}
+	}
+	//error
+}
+
+bool TechTree::unlockable(TechEnum tech)
+{
+	for (auto i : mTechTree)
+	{
+		if (i.techName == tech)
+		{
+			for (auto f : i.indexParents)
+			{
+				if (mTechTree[f].isUnclocked == false)
+					return false;
+			}
+			return true;
+		}
+	}
+	//error
+}
--- Classes/TechTree.h
+++ Classes/TechTree.h
@@ -7,41 +7,44 @@
 #include "json/document.h"
 #include "cocos2d.h"
 
-const std::map<std::string, TechEnum> mTechStringEnumMap = 
+namespace techTree
 {
-	{"techroot",techroot},
-	{"techtree1",techtree1},
-	{"techtree2",techtree2},
-	{"techtree3",techtree3},
-	{"unlocklongrangeunit1",unlocklongrangeunit1},
-	{"unlocklongrangeunit2",unlocklongrangeunit2},
-	{"unlockshortrangeunit1",unlockshortrangeunit1},
-	{"unlockshortrangeunit2",unlockshortrangeunit2},
-	{"unlocklongrangeunit3",unlocklongrangeunit3},
-	{"productivity1",productivity1},
-	{"productivity2",productivity2},
-	{"productivity3",productivity3},
-	{"resourcefixed1",resourcefixed1},
-	{"resourcefixed2",resourcefixed2},
-	{"resourcefixed3",resourcefixed3},
-	{"resourcerandom1",resourcerandom1},
-	{"resourcerandom2",resourcerandom2},
-	{"resourcerandom3",resourcerandom3},
-	{"hplongrange1",hplongrange1},
-	{"hplongrange2",hplongrange2},
-	{"arglongrange1",arglongrange1},
-	{"arglongrange2",arglongrange2},
-	{"atklongrange1",atklongrange1},
-	{"atklongrange2",atklongrange2},
-	{"atkshortrange1",atkshortrange1},
-	{"atkshortrange2",atkshortrange2},
-	{"defshortrange1",defshortrange1},
-	{"defshortrange2",defshortrange2},
-	{"mrgshortrange1",mrgshortrange1},
-	{"mrgshortrange2",mrgshortrange2},
-	{"defbase1",defbase1},
-	{"defbase2",defbase2}
-};
+	const std::map<std::string, TechEnum> mTechStringEnumMap =
+	{
+		{ "techroot", techroot },
+		{ "techtree1", techtree1 },
+		{ "techtree2", techtree2 },
+		{ "techtree3", techtree3 },
+		{ "unlocklongrangeunit1", unlocklongrangeunit1 },
+		{ "unlocklongrangeunit2", unlocklongrangeunit2 },
+		{ "unlockshortrangeunit1", unlockshortrangeunit1 },
+		{ "unlockshortrangeunit2", unlockshortrangeunit2 },
+		{ "unlocklongrangeunit3", unlocklongrangeunit3 },
+		{ "productivity1", productivity1 },
+		{ "productivity2", productivity2 },
+		{ "productivity3", productivity3 },
+		{ "resourcefixed1", resourcefixed1 },
+		{ "resourcefixed2", resourcefixed2 },
+		{ "resourcefixed3", resourcefixed3 },
+		{ "resourcerandom1", resourcerandom1 },
+		{ "resourcerandom2", resourcerandom2 },
+		{ "resourcerandom3", resourcerandom3 },
+		{ "hplongrange1", hplongrange1 },
+		{ "hplongrange2", hplongrange2 },
+		{ "arglongrange1", arglongrange1 },
+		{ "arglongrange2", arglongrange2 },
+		{ "atklongrange1", atklongrange1 },
+		{ "atklongrange2", atklongrange2 },
+		{ "atkshortrange1", atkshortrange1 },
+		{ "atkshortrange2", atkshortrange2 },
+		{ "defshortrange1", defshortrange1 },
+		{ "defshortrange2", defshortrange2 },
+		{ "mrgshortrange1", mrgshortrange1 },
+		{ "mrgshortrange2", mrgshortrange2 },
+		{ "defbase1", defbase1 },
+		{ "defbase2", defbase2 }
+	};
+}
 class TechTree
 {
 public:
