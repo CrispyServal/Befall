6a8b2859be438ecd1e9575d3ad0fb3c03ea25069
yypactive <yypactive@126.com>
2015-04-28 21:44:52 +0900

    GameTimer

--- Classes/GameTimer.h
+++ Classes/GameTimer.h
@@ -1,6 +1,6 @@
 #ifndef GAMETIMER_H
 #define GAMETIMER_H
-
+#include <string>
 #include "cocos2d.h"
 
 using namespace cocos2d;
@@ -11,11 +11,15 @@ public:
 	GameTimer();
 	~GameTimer();
 	virtual bool init();
-	//必须调用设置最大时间
-	void setMaxTime(int maxTime);
 	CREATE_FUNC(GameTimer);
+	//必须调用设置最大时间
+	void setMaxTime(float maxTime);
+	void setEndName(std::string endName);
 	//没点到的时候不能阻塞主地图
-	bool blockClick = false;
+	bool blockClick()
+	{
+		return false;
+	}
 	//只需判断结束回合有没有包含
 	bool containPoint(Vec2 mousePoint);
 	//关灯的函数：计时环不显示，结束半圆颜色变暗
@@ -24,14 +28,21 @@ public:
 	void start();
 	//返回是否时间到，时间到为true。GameScene的update中检查，结束的话强制结束回合。
 	bool isEnded();
+	void refresh(float deltaTime);
 private:
+	float width;
+	float  height;
 	//初始化的最大时间
-	int mMaxTime = 60;
+	float mMaxTime = 60;
 	//剩余时间
-	int mNowTime;
-	Sprite * mHalfRing;
+	float mNowTime;
+	Label * mEndLabel;
 	Sprite * mBgCircle;
+	Sprite * mCoverCircle;
 	Sprite * mEndTurnButton;
+	DrawNode * mVitualCircle;
+	bool mTimerState;
+	float radius;
 };
 
 #endif // !GAMETIMER_H
